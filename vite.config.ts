ecffd8b7ebed04929acbcf69089b238921bae1ed
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-10-05 15:05:31 +0530

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Your App Name",
+        short_name: "AppName",
+        description: "Your App Description",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/favicon.jpeg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/favicon.jpeg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
